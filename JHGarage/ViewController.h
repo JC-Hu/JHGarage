ec62dac765b59aae6da8e1bfc89e919c5cd82b0a
JasonHu <jchu_dlcn@icloud.com>
2018-11-22 22:30:39 +0800

    m

--- JHGarage/ViewController.h
+++ JHGarage/ViewController.h
@@ -1,6 +1,6 @@
 //
 //  ViewController.h
-//  JHGarageDemo
+//  JHGarage
 //
 //  Created by Jason Hu on 2018/11/22.
 //  Copyright © 2018 Jason Hu. All rights reserved.
--- JHGarage/ViewController.m
+++ JHGarage/ViewController.m
@@ -1,13 +1,12 @@
 //
 //  ViewController.m
-//  JHGarageDemo
+//  JHGarage
 //
 //  Created by Jason Hu on 2018/11/22.
 //  Copyright © 2018 Jason Hu. All rights reserved.
 //
 
 #import "ViewController.h"
-#import <JHGarage/JHGarage.h>
 
 @interface ViewController ()
 
@@ -18,8 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
-    [JHGarage test];
 }
 
 
